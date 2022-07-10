7560c68b79fa1f431be5ac1ac42de6e757d8ddf6
kb_swastik <76393434+Swastik-Sarkar@users.noreply.github.com>
2022-07-10 20:46:31 +0530

    Create HylexMC.java

---
+++ HylexClient[1.8.9]/me/kbs/hylex/HylexMC.java
@@ -0,0 +1,15 @@
+package me.kbs.hylex;
+
+import net.minecraft.command.ICommand;
+import me.kbs.hylex.wings.command.CommandEditWings;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "HylexClient", name = "Hylex Client", version = "1.2", acceptedMinecraftVersions = "[1.8.9]")
+public class hylex
+{
+}
